82a0f5d84a6034af5f09296fb89a0d389358e40e
Damir <70011787+SpeedFireF@users.noreply.github.com>
2023-11-11 03:46:37 +0300

    Add files via upload

---
+++ src/split.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def split_train_val(df: pd.DataFrame, features: list, labels: list, val_size: float, seed: int):
+    X = df[features]
+    y = df[labels]
+
+    # Split the data into training and validation sets
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=val_size, random_state=seed)
+
+    return X_train, X_val, y_train, y_val
