745e9101816f32fd9359f608e1994cfe183b9902
hir0github <almazdautov1234523@gmail.com>
2023-11-12 00:08:35 +0300

    solution improvement

--- train/utils.py
+++ train/utils.py
@@ -2,9 +2,23 @@
 import os
 import random
 import numpy as np
+from datetime import datetime
+import pytz
 
 
 def seed_everything(seed: int):
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
     np.random.seed(seed)
+
+
+def date_to_timestamp(date: str) -> int:
+    utc = pytz.UTC
+    date_utc = utc.localize(date)
+    timestamp_seconds = int(date_utc.timestamp())
+    return timestamp_seconds
+
+def timestamp_to_date(timestamp: int) -> str:
+    date = datetime.utcfromtimestamp(timestamp)
+    date_string = date.strftime('%Y-%m-%d')
+    return date_string
